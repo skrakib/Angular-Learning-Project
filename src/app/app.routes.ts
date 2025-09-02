680f9f52d283d80990526d0ee089ef8bb72a3a74
skrakib <ayfskm@gmail.com>
2025-09-03 00:31:34 +0600

    after Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Directive } from './components/directive/directive';
+import { DataBinding } from './components/data-binding/data-binding';
+import { Variables } from './components/variables/variables';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'directive',
+        component:Directive
+    },
+    {
+        path:'data-binding',
+        component:DataBinding
+    },
+    {
+        path:'variables',
+        component:Variables
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Variables } from "./components/variables/variables";
-import { DataBinding } from './components/data-binding/data-binding';
-import { Directive } from "./components/directive/directive";
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Variables, DataBinding, Directive],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
